2ed7bcffb82068b559131e397e380deeacefe623
Christopher Reaume <chris@desert.net>
2018-10-09 15:45:58 -0700

    Make topic_top a service.

--- etc/scripts/workstation/misa.pl
+++ etc/scripts/workstation/misa.pl
@@ -6,8 +6,7 @@ require 'workstation/config.pl';
 workstation_header();
 workstation_benchmarks();
 workstation_partitions();
-workstation_services();
-workstation_topic_top();
+# workstation_services();
 workstation_sound_effects();
 workstation_http_server();
 
