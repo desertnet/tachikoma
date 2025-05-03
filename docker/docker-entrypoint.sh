4fc973b9b870280207af7fd91678b3fa41a56c58
datapoke <7674597+datapoke@users.noreply.github.com>
2025-05-02 17:34:06 -0700

    Move build to docker-entrypoint.sh

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -3,9 +3,31 @@ if [ -n "$1" ] ; then
     exec $@
 fi
 
-cd /usr/src/tachikoma/
-su tachikoma -c "bin/install_tachikoma_user ${CONFIG}"
+if [ "${TACHIKOMA_UID}" = '' ]; then
+    TACHIKOMA_UID=1000
+fi
 
+# make user account
+useradd -s /bin/bash -u ${TACHIKOMA_UID} -d /home/tachikoma tachikoma
+
+# copy source
+cp -rfT /services/tachikoma /usr/src/tachikoma
+
+# make tachikoma log dirs
+mkdir -p /home/tachikoma/.tachikoma/log
+chown -R ${PUB_ID}:${PUB_ID} /home/tachikoma/.tachikoma/log
+cd /home/tachikoma/.tachikoma/log
+
+# install tachikoma
+cd /usr/src/tachikoma \
+    && bin/install_tachikoma
+
+# install tachikoma user configuration
+cd /usr/src/tachikoma/
+su ${PUB_ID} -c "bin/install_tachikoma_user ${CONFIG}"
+
+# clear stale pid files
 rm -f /home/tachikoma/.tachikoma/run/*
 
-exec su - tachikoma -c "/usr/local/bin/tachikoma-server --daemon=no"
+# start service
+exec su - ${PUB_ID} -c "/usr/local/bin/tachikoma-server --daemon=no"
