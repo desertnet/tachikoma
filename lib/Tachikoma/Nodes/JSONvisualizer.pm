379e2d258a4e028ae2f8a0dc55476e84109c745a
datapoke <7674597+datapoke@users.noreply.github.com>
2025-07-24 21:27:58 -0700

    Warm things up a bit now that it's working well.

--- lib/Tachikoma/Nodes/JSONvisualizer.pm
+++ lib/Tachikoma/Nodes/JSONvisualizer.pm
@@ -41,46 +41,45 @@ sub fill {
     my $self    = shift;
     my $message = shift;
     return if ( not $message->[TYPE] & TM_STORABLE );
-    my $path  = $message->payload->{path};
-    my $nodes = undef;
+    my $path    = $message->payload->{path};
+    my $content = undef;
     $path =~ s{^/+}{};
     if ( $path eq 'brief' ) {
-        $nodes = $self->gather_node_information('brief');
+        $content = $self->gather_node_information('brief');
     }
     else {
-        $nodes = $self->gather_node_information('full');
+        $content = $self->gather_node_information('full');
     }
-    my $content = encode_json($nodes);
-    $self->send_http_response( $message, \$content );
+    $self->send_http_response( $message, $content );
     $self->{counter}++;
     log_entry( $self, 200, $message );
     return;
 }
 
 sub gather_node_information {
-    my $self  = shift;
-    my $mode  = shift;
-    my $nodes = [];
-    if ( $self->{last_update} > $Tachikoma::Now - 15 ) {
+    my $self    = shift;
+    my $mode    = shift;
+    my $content = undef;
+    if ( $self->{last_update} > $Tachikoma::Now - 5 ) {
         if ( $mode eq 'full' ) {
-            $nodes = $self->full_cache;
+            $content = $self->full_cache;
         }
         else {
-            $nodes = $self->brief_cache;
+            $content = $self->brief_cache;
         }
     }
     else {
-        $nodes = $self->_gather_node_information($mode);
+        $content = $self->_gather_node_information($mode);
     }
-    return $nodes;
+    return $content;
 }
 
 sub _gather_node_information {
     my $self        = shift;
     my $mode        = shift;
-    my $brief_cache = [];
-    my $full_cache  = [];
-    my $nodes       = undef;
+    my $brief_table = [];
+    my $full_table  = [];
+    my $content     = undef;
     my $ignore      = $self->{arguments};
     my %ids         = ();
     my $id          = 0;
@@ -111,29 +110,31 @@ sub _gather_node_information {
             my $name = ( split m{/}, $node->{owner}, 2 )[0];
             $owner = $ids{$name} if ( exists $ids{$name} );
         }
-        my $brief_info = {
+        my $brief_row = {
             sink    => $node->{sink} ? $ids{ $node->{sink}->{name} } : -1,
             edge    => $node->{edge} ? $ids{ $node->{edge}->{name} } : -1,
             owner   => $owner // -1,
             counter => $node->{counter} ? $node->{counter} : 0,
         };
-        push @{$brief_cache}, $brief_info;
-        my $full_info = {
+        push @{$brief_table}, $brief_row;
+        my $full_row = {
             name => $name,
-            %{$brief_info},
+            %{$brief_row},
         };
-        push @{$full_cache}, $full_info;
+        push @{$full_table}, $full_row;
     }
-    $self->full_cache($full_cache);
-    $self->brief_cache($brief_cache);
+    my $full_json  = encode_json($full_table);
+    my $brief_json = encode_json($brief_table);
+    $self->full_cache( \$full_json );
+    $self->brief_cache( \$brief_json );
     $self->last_update($Tachikoma::Now);
     if ( $mode eq 'full' ) {
-        $nodes = $full_cache;
+        $content = \$full_json;
     }
     else {
-        $nodes = $brief_cache;
+        $content = \$brief_json;
     }
-    return $nodes;
+    return $content;
 }
 
 sub send_http_response {
